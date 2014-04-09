088600861ca5c87e96b67450500abd86bd9c53ab
vcruzmj <manuel@odevc.com>
2014-04-09 22:48:54 +0000

    agregado un label

--- Lab02Bb/src/Ventana.java
+++ Lab02Bb/src/Ventana.java
@@ -1,16 +1,14 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 
 public class Ventana {
 public static void main(String[] args) {
 		
 		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
+		label.setText("Hola soy una Label");
+		frame.add(label);
 		frame.setTitle("Mi segunda ventana");
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
