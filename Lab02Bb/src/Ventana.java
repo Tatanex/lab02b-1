c54ddc62b8b84059bc78c7af7ef065e6ba53d14e
vcruzmj <manuel@odevc.com>
2014-04-09 22:55:46 +0000

    Agregado un boton

--- Lab02Bb/src/Ventana.java
+++ Lab02Bb/src/Ventana.java
@@ -1,5 +1,10 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 
 public class Ventana {
@@ -9,6 +14,14 @@ public static void main(String[] args) {
 		JLabel label = new JLabel();
 		label.setText("Hola soy una Label");
 		frame.add(label);
+		JButton button = new JButton();
+		button.setText("Presioname!");
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "PRESIONADO");
+			}});
+		frame.add(button);
 		frame.setTitle("Mi segunda ventana");
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
