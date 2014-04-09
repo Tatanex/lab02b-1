f0fcc94320ddcaa7ce67a681f491f1f269450982
vcruzmj <manuel@odevc.com>
2014-04-09 22:36:46 +0000

    Creacion ventana

---
+++ Lab02Bb/src/Ventana.java
@@ -0,0 +1,22 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+
+public class Ventana {
+public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		frame.setTitle("Mi primera ventana");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	
+
+	}
+}
